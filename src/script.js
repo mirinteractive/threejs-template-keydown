7592aac3c6bd0c25e2021a31a7d69d9062226448
miri8D <inadvanced8282@gmail.com>
2021-09-13 20:31:35 +0900

    Apply proper code with proper scene setting

--- src/script.js
+++ src/script.js
@@ -43,7 +43,7 @@ scene.add(ambientLight)
 
 //directional light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
-directionalLight.position.set(1, 0.25, 0)
+directionalLight.position.set(0, 0.25, 0)
 scene.add(directionalLight)
 
 /**
@@ -139,8 +139,9 @@ function objectColisionSphere(container, sphere, mass) {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-//camera collision test
-camera.position.set(3, 1, 5)
+camera.position.set(-30, 1, 10)
+//proper rotation point
+camera.rotation.set(0, -Math.PI*0.3, 0)
 scene.add(camera)
 cameraCollisionBox(2, 2, 2, { x: 0, y: 0, z: 0 })
 
@@ -150,12 +151,16 @@ cameraCollisionBox(2, 2, 2, { x: 0, y: 0, z: 0 })
 //floor
 const floorMaterial = new THREE.MeshStandardMaterial({
     roughness: 0.4,
-    color: '#F4ECD6', 
+    color: '#FFF1D0', 
+})
+const wallMaterial = new THREE.MeshStandardMaterial({
+    roughness: 0.4,
+    color: '#06AED5', 
 })
 
-const floor = new THREE.Mesh(new THREE.PlaneGeometry(50, 50),floorMaterial)
+const floor = new THREE.Mesh(new THREE.PlaneGeometry(80, 50),floorMaterial)
 floor.rotation.set(-Math.PI*0.5, 0, 0)
-floor.position.set(0, -0.65, 0)
+floor.position.set(0, -1, 0)
 const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body({
     mass: floorMass,
@@ -168,64 +173,135 @@ floorBody.quaternion.setFromAxisAngle(
 world.addBody(floorBody)
 scene.add(floor)
 
-const wallFront = new THREE.Mesh(new THREE.BoxGeometry(50, 10, 1))
-wallFront.position.set(0, 0, -20)
+const wallFront = new THREE.Mesh(new THREE.BoxGeometry(80, 20, 1),wallMaterial)
+wallFront.position.set(0, 0, -24)
 const wallFrontContainer = []
 const wallFrontCollision = new objectColisionBox(wallFrontContainer, wallFront, floorMass)
 wallFrontCollision.createBox()
 
-const wallBack = new THREE.Mesh(new THREE.BoxGeometry(50, 10, 1))
-wallBack.position.set(0, 0, 20)
+const wallBack = new THREE.Mesh(new THREE.BoxGeometry(80, 20, 1),wallMaterial)
+wallBack.position.set(0, 0, 25)
 const wallBackContainer = []
 const wallBackCollision = new objectColisionBox(wallBackContainer, wallBack, floorMass)
 wallBackCollision.createBox()
 
-const wallRight = new THREE.Mesh(new THREE.BoxGeometry(1, 10, 50))
-wallRight.position.set(20, 0, 0)
+const wallRight = new THREE.Mesh(new THREE.BoxGeometry(1, 20, 50),wallMaterial)
+wallRight.position.set(40, 0, 0)
 const wallRightContainer = []
 const wallRightCollision = new objectColisionBox(wallRightContainer, wallRight, floorMass)
 wallRightCollision.createBox()
 
-const wallLeft = new THREE.Mesh(new THREE.BoxGeometry(1, 10, 50))
-wallLeft.position.set(-20, 0, 0)
+const wallLeft = new THREE.Mesh(new THREE.BoxGeometry(1, 20, 50),wallMaterial)
+wallLeft.position.set(-39, 0, -10)
 const wallLeftContainer = []
 const wallLeftCollision = new objectColisionBox(wallLeftContainer, wallLeft, floorMass)
 wallLeftCollision.createBox()
 
+//door position
+const doorMaterial = new THREE.MeshStandardMaterial({
+    roughness: 0.4,
+    color: '#000000', 
+})
+
+const doorToNews = new THREE.Mesh(new THREE.BoxGeometry(5, 10, 0.1),doorMaterial)
+doorToNews.position.set(-20, 3, -24)
+const doorToTtv = new THREE.Mesh(new THREE.BoxGeometry(0.1, 10, 5),doorMaterial)
+doorToTtv.position.set(39, 3, -5)
+const doorToVtr = new THREE.Mesh(new THREE.BoxGeometry(0.1, 10, 5),doorMaterial)
+doorToVtr.position.set(39, 3, 2)
+const doorToPicasso = new THREE.Mesh(new THREE.BoxGeometry(0.1, 10, 5),doorMaterial)
+doorToPicasso.position.set(39, 3, 19)
+const doorToPortfolio = new THREE.Mesh(new THREE.BoxGeometry(0.1, 10, 5),doorMaterial)
+doorToPortfolio.position.set(-39, 3, -19)
+const doorToMediaTeam = new THREE.Mesh(new THREE.BoxGeometry(0.1, 10, 5),doorMaterial)
+doorToMediaTeam.position.set(-39, 3, 19)
+scene.add(doorToNews, doorToTtv, doorToVtr, doorToPicasso, doorToPortfolio, doorToMediaTeam)
+
 //objects
 const objectMaterial = new THREE.MeshStandardMaterial({
     roughness: 0.4,
-    color: '#88B7B5', 
+    color: '#086788', 
 })
+const monumentMaterial = new THREE.MeshStandardMaterial({
+    roughness: 0.4,
+    color: '#DD1C1A', 
+})
+
+const wallOfFame = new THREE.Mesh(new THREE.BoxGeometry(15, 8, 0.3),objectMaterial)
+wallOfFame.position.set(-9, 4, -24)
+const wallOfMomentum = new THREE.Mesh(new THREE.BoxGeometry(6, 8, 0.3),objectMaterial)
+wallOfMomentum.position.set(3, 4, -24)
+//
+const grpupIntro = new THREE.Group();
+grpupIntro.add(wallOfFame, wallOfMomentum)
+
+const wallOfTtvVideo1 = new THREE.Mesh(new THREE.BoxGeometry(0.3, 3, 5),objectMaterial)
+wallOfTtvVideo1.position.set(39, 7, -3)
+const stickerOfTtvPdf = new THREE.Mesh(new THREE.BoxGeometry(0.3, 1.5, 1.5),objectMaterial)
+stickerOfTtvPdf.position.set(39, 4.5, -1.3)
+const wallOfTtvVideo2 = new THREE.Mesh(new THREE.BoxGeometry(0.3, 3, 5),objectMaterial)
+wallOfTtvVideo2.position.set(39, 2, -0.8)
+//
+const grpupTtv = new THREE.Group();
+grpupTtv.add(wallOfTtvVideo1, wallOfTtvVideo2, stickerOfTtvPdf)
+grpupTtv.position.set(0, -0.9, -10)
+
+const stickerOfFb = new THREE.Mesh(new THREE.BoxGeometry(1.5, 1.5, 0),objectMaterial)
+stickerOfFb.position.set(-32, 5, 24)
+const stickerOfIg= new THREE.Mesh(new THREE.BoxGeometry(1.5, 1.5, 0),objectMaterial)
+stickerOfIg.position.set(-35, 4, 24)
+const stickerOfYt= new THREE.Mesh(new THREE.BoxGeometry(1.5, 1.5, 0),objectMaterial)
+stickerOfYt.position.set(-33, 3, 24)
+//
+const grpupSns = new THREE.Group();
+grpupSns.add(stickerOfFb, stickerOfIg, stickerOfYt)
+
+const stickerOfTech= new THREE.Mesh(new THREE.BoxGeometry(1.5, 1.5, 0),objectMaterial)
+stickerOfTech.position.set(32, 5, 24)
+const stickerOfArch = new THREE.Mesh(new THREE.BoxGeometry(1.5, 1.5, 0),objectMaterial)
+stickerOfArch.position.set(29, 4, 24)
+const stickerOfIr = new THREE.Mesh(new THREE.BoxGeometry(1.5, 1.5, 0),objectMaterial)
+stickerOfIr.position.set(31, 3, 24)
+//
+const grpupDock = new THREE.Group();
+grpupSns.add(stickerOfTech, stickerOfArch, stickerOfIr)
+
+const monumentLogoBot = new THREE.Mesh(new THREE.BoxGeometry(1, 2, 1), monumentMaterial)
+monumentLogoBot.position.set(0, 0, 0)
+const monumentLogoTop = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), monumentMaterial)
+monumentLogoTop.position.set(0, 2, 0)
+//
+const grpupLogo = new THREE.Group();
+grpupLogo.add(monumentLogoBot, monumentLogoTop)
+grpupLogo.position.set(-28, 0, 0)
+
+const monumentContactBot = new THREE.Mesh(new THREE.BoxGeometry(1, 2, 1), monumentMaterial)
+monumentContactBot.position.set(0, 0, 0)
+const monumentContactTop = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 32, 64), monumentMaterial)
+monumentContactTop.position.set(0, 2, 0)
+//
+const grpupContact = new THREE.Group();
+grpupContact.add(monumentContactBot, monumentContactTop)
+grpupContact.position.set(10, 0, -13)
+
+const monumentTeamBot = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), monumentMaterial)
+monumentTeamBot.position.set(0, 0, 0)
+const monumentTeamMid = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), monumentMaterial)
+monumentTeamMid.position.set(0, 1, 0)
+const monumentTeamTop = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), monumentMaterial)
+monumentTeamTop.position.set(0, 2, 0)
+//
+const grpupTeam = new THREE.Group();
+grpupTeam.add(monumentTeamBot, monumentTeamMid, monumentTeamTop)
+grpupTeam.position.set(-10, 0, 10)
+
+scene.add(grpupIntro, grpupTtv, grpupSns, grpupDock, grpupLogo, grpupContact, grpupTeam);
 
 const sphereBall = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), objectMaterial)
 sphereBall.position.set(1.5, 0, 0)
 const sphereBallContainer = []
 const sphereBallCollision = new objectColisionSphere(sphereBallContainer, sphereBall, objectMass)
 sphereBallCollision.createSphere()
-
-const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), objectMaterial)
-cube.position.set(3, 1, -1)
-
-const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 3, 1), objectMaterial)
-cube2.position.set(1, 0, 1)
-const cube2Container = []
-const testCube = new objectColisionBox(cube2Container, cube2, objectMass)
-testCube.createBox()
-
-const cubeTest = new THREE.Mesh(new THREE.BoxGeometry(1, 2, 1), objectMaterial)
-cubeTest.position.set(-3, 0, 0)
-
-const boxGeometry = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), objectMaterial)
-boxGeometry.position.set(5, 0, 1)
-const objectsToUpdate = []
-const testCube2 = new objectColisionBox(objectsToUpdate, boxGeometry, objectMass)
-testCube2.createBox()
-
-const torus = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 32, 64), objectMaterial)
-torus.position.set(-1.5, 0, 0) 
-
-scene.add(cube, torus, cubeTest)
 
 /**
  * Models
@@ -255,7 +331,7 @@ gltfLoader.load(gltfURL,(gltf) => {
 // console.log('gltfObjectContainer', gltfObjectContainer.body);
 // console.log('gltfObjectContainer', Object.values(gltfObjectContainer));
 
-// //fbx
+//fbx
 //  let fbxURL = "/models/fbx/exportfbx_standard.fbx"; 
 //  const fbxLoader = new FBXLoader(); 
 //  fbxLoader.load(fbxURL,(fbx) => {
@@ -281,7 +357,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock();
 let controlCamera = new PointerLockControls(camera, renderer.domElement);
 let button = document.querySelector('#button')
-let speed = 0.2;
+let speed = 0.3;
 let keyboard = []
 
 button.addEventListener('click', () => {
@@ -338,10 +414,10 @@ const tick = () =>
     wallLeftContainer[0].box.position.copy(wallLeftContainer[0].body.position)
     floorBody.position.copy(floor.position)
     sphereBallContainer[0].sphere.position.copy(sphereBallContainer[0].body.position)
-    cube2Container[0].box.position.copy(cube2Container[0].body.position)
-    objectsToUpdate[0].box.position.copy(objectsToUpdate[0].body.position)
-    // gltfObjectContainer[0].box.position.copy(gltfObjectContainer[0].body.position)
-
+    // cube2Container[0].box.position.copy(cube2Container[0].body.position)
+    // cube2Container[0].box.quaternion.copy(cube2Container[0].body.quaternion)
+    // objectsToUpdate[0].box.position.copy(objectsToUpdate[0].body.position)
+    // objectsToUpdate[0].box.quaternion.copy(objectsToUpdate[0].body.quaternion)
 
     /**
     * Raycaster
@@ -386,11 +462,12 @@ const tick = () =>
     }
 
     // Update objects
-    cube.rotation.y = 0.1 * elapsedTime
-    torus.rotation.y = 0.1 * elapsedTime
+    monumentLogoTop.rotation.y = 0.1 * elapsedTime
+    monumentLogoTop.rotation.x = 0.15 * elapsedTime 
 
-    cube.rotation.x = 0.15 * elapsedTime 
-    torus.rotation.x = 0.15 * elapsedTime
+    monumentContactTop.rotation.y = 0.5 * elapsedTime
+    monumentContactTop.rotation.x = 0.15 * elapsedTime
+    monumentContactTop.rotation.z = -0.15 * elapsedTime
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
