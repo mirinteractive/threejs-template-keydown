e059581752c5c11ecde7bf196b208c842faa5600
miri8D <inadvanced8282@gmail.com>
2021-12-10 23:07:31 +0900

    add collision logic

--- src/objects.js
+++ src/objects.js
@@ -3,12 +3,24 @@ import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 
 let fbxURL = "/models/fbx/sample.fbx"; 
 const fbxLoader = new FBXLoader(); 
-function loadMoldeledObjects(sceneTest) {
+function loadMoldeledObjects(scene, worldOctree) {
     fbxLoader.load(fbxURL,(fbx) => {
         fbx.scale.set(0.01, 0.01, 0.01)
         fbx.position.set(0,-1,23)
         fbx.rotation.set(0, Math.PI*0.5, 0)
-        sceneTest.add(fbx)
+        scene.add(fbx)
+
+        worldOctree.fromGraphNode( fbx );
+        fbx.traverse( child => {
+            if ( child.isMesh ) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+    
+                if ( child.material.map ) {
+                    child.material.map.anisotropy = 8;
+                }
+            }
+        });
         }
      )
 }
--- src/script.js
+++ src/script.js
@@ -1,99 +1,170 @@
 import './style.css'
 import * as THREE from 'three'
-import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js'
+import { Octree } from 'three/examples/jsm/math/Octree.js';
+import { Capsule } from 'three/examples/jsm/math/Capsule.js';
 import * as environment from './environment'
 import * as objects from './objects'
 
 const canvas = document.querySelector('canvas.webgl');
+
+const clock = new THREE.Clock();
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0x88ccff );
- const sizes = {
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.rotation.order = 'YXZ';
+camera.position.set(3,1.5,5)
+camera.rotation.set(0, -Math.PI, 0)
+scene.add(camera)
+
+const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
-window.addEventListener('resize', () =>
-{
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
 
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+function onWindowResize() {
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
 
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+window.addEventListener( 'resize', onWindowResize );
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-scene.add(camera)
-
-/**
- * Renderer
- */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * FPS View
- */
-const clock = new THREE.Clock();
-let controlCamera = new PointerLockControls(camera, renderer.domElement);
-let button = document.querySelector('#button')
-let speed = 0.3;
-let keyboard = []
+const GRAVITY = 30;
+const STEPS_PER_FRAME = 5;
 
-button.addEventListener('click', () => {
-    console.log('ViewStart');
-    controlCamera.lock()
-})
+const worldOctree = new Octree();
 
-addEventListener('keydown', (e)=> {
-    keyboard[e.key] = true;
-})
-addEventListener('keyup', (e)=> {
-    keyboard[e.key] = false;
-})
+const playerCollider = new Capsule( new THREE.Vector3( 0, 0.35, 0 ), new THREE.Vector3( 0, 1, 0 ), 0.35 );
+const playerVelocity = new THREE.Vector3();
+const playerDirection = new THREE.Vector3();
+const keyStates = {};
 
-function processKeyboard() {
-    if(keyboard['w'] || keyboard['ArrowUp']) {
-        controlCamera.moveForward(speed);
+let playerOnFloor = false;
+let mouseTime = 0;
+
+document.addEventListener( 'keydown', ( event ) => {
+    keyStates[ event.code ] = true;
+});
+
+document.addEventListener( 'keyup', ( event ) => {
+    keyStates[ event.code ] = false;
+});
+
+document.addEventListener( 'mousedown', () => {
+    document.body.requestPointerLock();
+    mouseTime = performance.now();
+});
+
+document.body.addEventListener( 'mousemove', ( event ) => {
+    if ( document.pointerLockElement === document.body ) {
+        camera.rotation.y -= event.movementX / 500;
+        camera.rotation.x -= event.movementY / 500;
     }
-    if(keyboard['s'] || keyboard['ArrowDown']) {
-        controlCamera.moveForward(-speed);
+});
+
+function playerCollisions() {
+    const result = worldOctree.capsuleIntersect( playerCollider );
+    playerOnFloor = false;
+
+    if ( result ) {
+        playerOnFloor = result.normal.y > 0;
+        if ( ! playerOnFloor ) {
+            playerVelocity.addScaledVector( result.normal, - result.normal.dot( playerVelocity ) );
+        }
+        playerCollider.translate( result.normal.multiplyScalar( result.depth ) );
     }
-    if(keyboard['d'] || keyboard['ArrowRight']) {
-        controlCamera.moveRight(speed);
+}
+
+function updatePlayer( deltaTime ) {
+    let damping = Math.exp( - 4 * deltaTime ) - 1;
+    if ( ! playerOnFloor ) {
+        playerVelocity.y -= GRAVITY * deltaTime;
+        damping *= 0.1;
     }
-    if(keyboard['a'] || keyboard['ArrowLeft']) {
-        controlCamera.moveRight(-speed);
+
+    playerVelocity.addScaledVector( playerVelocity, damping );
+
+    const deltaPosition = playerVelocity.clone().multiplyScalar( deltaTime );
+    playerCollider.translate( deltaPosition );
+
+    playerCollisions();
+
+    camera.position.copy( playerCollider.end );
+}
+
+function getForwardVector() {
+    camera.getWorldDirection( playerDirection );
+    playerDirection.y = 0;
+    playerDirection.normalize();
+
+    return playerDirection;
+}
+
+function getSideVector() {
+    camera.getWorldDirection( playerDirection );
+    playerDirection.y = 0;
+    playerDirection.normalize();
+    playerDirection.cross( camera.up );
+
+    return playerDirection;
+}
+
+function controls( deltaTime ) {
+    const speedDelta = deltaTime * ( playerOnFloor ? 25 : 8 );
+
+    if ( keyStates[ 'KeyW' ] ) {
+        playerVelocity.add( getForwardVector().multiplyScalar( speedDelta ) );
+    }
+
+    if ( keyStates[ 'KeyS' ] ) {
+        playerVelocity.add( getForwardVector().multiplyScalar( - speedDelta ) );
+    }
+
+    if ( keyStates[ 'KeyA' ] ) {
+        playerVelocity.add( getSideVector().multiplyScalar( - speedDelta ) );
+    }
+
+    if ( keyStates[ 'KeyD' ] ) {
+        playerVelocity.add( getSideVector().multiplyScalar( speedDelta ) );
+    }
+
+    if ( playerOnFloor ) {
+        if ( keyStates[ 'Space' ] ) {
+            playerVelocity.y = 15;
+        }
     }
 }
 
 environment.basicSceneAdd(scene)
-objects.loadMoldeledObjects(scene)
+// objects.loadGltfModel(scene, worldOctree)
+objects.loadMoldeledObjects(scene, worldOctree)
 
 /**
  * Animate
  */
 const tick = () =>
 {
-    //time
-    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = Math.min( 0.05, clock.getDelta() ) / STEPS_PER_FRAME;
 
-    // Call tick again on the next frame
+    for ( let i = 0; i < STEPS_PER_FRAME; i ++ ) {
+        controls( deltaTime );
+        updatePlayer( deltaTime );
+    }
+
     window.requestAnimationFrame(tick)
 }
 tick()
 
 function drawScene() { 
     renderer.render(scene, camera);
-    processKeyboard();
     requestAnimationFrame(drawScene);
 }
 drawScene();
